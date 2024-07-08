51c5abc582c6790750c81cfb4d4d452fad5caa5b
Mustapha GHLISSI <ghlissi.mustapha@gmail.com>
2024-07-08 11:42:15 +0100

    Feat: igone generated lib by bob

--- lib/typescript/components/Dropdown.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { DropdownProps } from './Dropdown.types';
-declare const Dropdown: React.FC<DropdownProps>;
-export default Dropdown;
-//# sourceMappingURL=Dropdown.d.ts.map
